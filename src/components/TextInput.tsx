73f23088cb90eccfaf4e711fa2c4e07c17e9d420
Eduardo Conde <emc@contentguru.com>
2025-01-03 08:30:46 +0000

    add focus dismiss

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,7 +1,7 @@
 import TextField from '@mui/material/TextField';
 import type { TextFieldProps } from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import type { FieldError } from 'react-hook-form';
 
 type TextInputProps = TextFieldProps & {
@@ -9,13 +9,27 @@ type TextInputProps = TextFieldProps & {
 };
 
 export const TextInput: FC<TextInputProps> = ({ formError, ...props }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <Tooltip
       title={formError?.message ?? ''}
+      open={!!formError && isFocused}
       placement="bottom"
       arrow
-      open={!!formError}>
-      <TextField {...props} error={!!formError} />
+      slotProps={{
+        tooltip: {
+          sx: {
+            width: '220px',
+          },
+        },
+      }}>
+      <TextField
+        {...props}
+        error={!!formError}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
     </Tooltip>
   );
 };
