0d2b5b09c42b011f6c8d6ae4665970b4e0d2dff1
Whandryn <whandryn@protonmai.com>
2025-01-19 15:03:33 +0000

    small fix

--- src/CoverBoard/Keyboard/MainKeyboardListener.tsx
+++ src/CoverBoard/Keyboard/MainKeyboardListener.tsx
@@ -1,92 +1,37 @@
-import { useAtomValue, useSetAtom } from 'jotai';
+import { useSetAtom } from 'jotai';
 import { FC, useEffect } from 'react';
 
-import {
-  editTitleAtom,
-  editingTextAtom,
-  hideToolbarAtom,
-  pointsAtom,
-  selectedAtom,
-  useShallowMainStore,
-} from 'store';
+import { hideToolbarAtom, useShallowMainStore } from 'store';
 import { KeyboardShortcuts } from 'types';
 import { usePreventKeys } from 'utils';
 
 export const MainKeyboardListener: FC = () => {
   const preventKeys = usePreventKeys();
 
-  const { fitToScreen, updateConfigs, getCovers, getGroups } =
-    useShallowMainStore((state) => ({
-      fitToScreen: state.configs.layout.fitToScreen,
-      updateConfigs: state.updateConfigs,
-      getCovers: state.getCovers,
-      getGroups: state.getGroups,
-    }));
-
-  const {
-    setHideToolBar,
-    setSelected,
-    points,
-    editTitle,
-    isTextSelected,
-    selected,
-  } = {
-    setHideToolBar: useSetAtom(hideToolbarAtom),
-    setSelected: useSetAtom(selectedAtom),
-    points: useAtomValue(pointsAtom),
-    editTitle: useAtomValue(editTitleAtom),
-    isTextSelected: !!useAtomValue(editingTextAtom),
-    selected: useAtomValue(selectedAtom),
-  };
-  const hasMode = !!points || !!selected || editTitle || !!isTextSelected;
+  const { fitToScreen, updateConfigs } = useShallowMainStore((state) => ({
+    fitToScreen: state.configs.layout.fitToScreen,
+    updateConfigs: state.updateConfigs,
+  }));
+  const setHideToolBar = useSetAtom(hideToolbarAtom);
 
   useEffect(() => {
     if (preventKeys) return;
 
     const keyFn = (e: KeyboardEvent) => {
-      const covers = getCovers();
-      const groups = getGroups();
+      const key = e.key as KeyboardShortcuts;
 
-      if ((e.key as KeyboardShortcuts) === KeyboardShortcuts.HIDE_TOOLBAR) {
+      if (key === KeyboardShortcuts.HIDE_TOOLBAR) {
         setHideToolBar((prev) => !prev);
         e.preventDefault();
-      } else if (
-        (e.key as KeyboardShortcuts) === KeyboardShortcuts.FIT_SCREEN
-      ) {
+      } else if (key === KeyboardShortcuts.FIT_SCREEN) {
         updateConfigs({ layout: { fitToScreen: !fitToScreen } });
         e.preventDefault();
-      } else if (
-        (e.key as KeyboardShortcuts) === KeyboardShortcuts.NEXT &&
-        !hasMode
-      ) {
-        if (covers.length > 0) {
-          setSelected({
-            id: covers[covers.length - 1].id,
-            open: false,
-          });
-          e.preventDefault();
-        } else if (groups.length > 0) {
-          setSelected({
-            id: groups[groups.length - 1].id,
-            open: false,
-          });
-          e.preventDefault();
-        }
       }
     };
     window.addEventListener('keydown', keyFn);
 
     return () => window.removeEventListener('keydown', keyFn);
-  }, [
-    fitToScreen,
-    getCovers,
-    getGroups,
-    hasMode,
-    preventKeys,
-    setHideToolBar,
-    setSelected,
-    updateConfigs,
-  ]);
+  }, [fitToScreen, preventKeys, setHideToolBar, updateConfigs]);
 
   return null;
 };
